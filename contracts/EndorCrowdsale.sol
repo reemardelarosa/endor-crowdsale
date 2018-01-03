f9fbbf6db5becb0c8caee8b83aff31000d2523a7
Lior Regev <lior@endor.com>
2018-01-03 14:08:10 +0200

    Copying OpenZeppelin crowdsale contracts to our repo for editing

--- contracts/EndorCrowdsale.sol
+++ contracts/EndorCrowdsale.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol";
+import "./RefundableCrowdsale.sol";
 import "./EndorToken.sol";
 
 contract EndorCrowdsale is RefundableCrowdsale {
